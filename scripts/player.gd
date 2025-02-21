d9d0adec92c9634fee42cf1ea36e412563425f78
David Ward <davidward462@users.noreply.github.com>
2025-02-20 22:11:28 -0800

    draft of player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,12 +9,16 @@ extends CharacterBody2D
 @onready var screen_size = get_viewport_rect().size
 
 var rotation_speed = 3.0
+
+var force = 3.0
+var drag_vector = Vector2.ZERO
+var drag_coef = 0.01
+
 var v = Vector2.ZERO
 var acceleration = 1.0
 var drag = 0.5
 var speed = 0.0
-var reverse_vector = Vector2.ZERO
-var drag_coef = 0.2
+
 const MAX_SPEED = 5.0
 
 func _do_rotation(delta: float) -> void:
@@ -34,16 +38,14 @@ func _physics_process(delta: float) -> void:
 	_do_rotation(delta)
 	
 	if Input.is_action_pressed("accelerate"):
-		if speed < MAX_SPEED:
-			speed += acceleration
-		
-		velocity += Vector2(speed, 0).rotated(rotation)
+		# when there is thrust, add force to velocity vector.
+		velocity += Vector2(force, 0).rotated(rotation)
 	else:
-		#if speed > 0:
-		#	
-		if speed > 0:
-			speed -= drag
-			velocity += Vector2(-1 * speed, 0).rotated(rotation)
+		# When there is no thrust, add the drag vector against the velocity.
+		# TODO: do somethign better than drag coefficient. Maybe a small threshold.
+		
+		drag_vector = -1 * velocity * drag_coef
+		velocity += drag_vector
 		
 
 	#move_and_slide()
