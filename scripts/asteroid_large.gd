1f143388e6dd8187c8a7a055d3c160e8a5119768
David Ward <davidward462@users.noreply.github.com>
2025-02-21 19:15:21 -0800

    add random movement to asteroids

--- scripts/asteroid_large.gd
+++ scripts/asteroid_large.gd
@@ -1,17 +1,28 @@
 extends CharacterBody2D
 
-@onready var rotation_label: Label = $RotationLabel
+@onready var screen_size = get_viewport_rect().size
 
-const SPEED = 10.0
+# TODO: remove after testing
+@onready var rotation_label: Label = $RotationLabel
+@onready var velocity_label: Label = $VelocityLabel
+
+var speed = 10.0
 var dir = 0.0
 
 func _ready() -> void:
 	randomize()
 	dir = randf_range(PI, -PI)
+	speed = randf_range(20.0, 130.0)
+	velocity = Vector2(speed, 0).rotated(dir)
 	
 func _process(delta: float) -> void:
 	rotation_label.text = str(dir)
+	velocity_label.text = str(velocity)
 
 func _physics_process(delta: float) -> void:
 	
 	move_and_collide(velocity * delta)
+	
+	# Wrap position on screen bounds
+	position.x = wrapf(position.x, 0, screen_size.x)
+	position.y = wrapf(position.y, 0, screen_size.y)
