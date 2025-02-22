5e1d43772c26e7a834cf7224d7ac3ef7fbe6a2e4
David Ward <davidward462@users.noreply.github.com>
2025-02-21 18:58:43 -0800

    draft asteroid code and add testing label

--- scripts/asteroid_large.gd
+++ scripts/asteroid_large.gd
@@ -1,8 +1,16 @@
 extends CharacterBody2D
 
+@onready var rotation_label: Label = $RotationLabel
 
-const SPEED = 300.0
+const SPEED = 10.0
+var dir = 0.0
 
+func _ready() -> void:
+	randomize()
+	dir = randf_range(PI, -PI)
+	
+func _process(delta: float) -> void:
+	rotation_label.text = str(dir)
 
 func _physics_process(delta: float) -> void:
 	
