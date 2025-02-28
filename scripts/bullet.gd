20cae36c9c8f1aa0d094921ce867ef065c7a2508
David Ward <davidward462@users.noreply.github.com>
2025-02-27 17:26:19 -0800

    add energy decay to bullet

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
 var speed = 700
+var energy = 100
+
+@onready var screen_size = get_viewport_rect().size
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,6 +12,14 @@ func _ready() -> void:
 # Move bullet according to speed
 func _physics_process(delta: float) -> void:
 	position += transform.x * speed * delta
+	
+	# Wrap position on screen bounds
+	position.x = wrapf(position.x, 0, screen_size.x)
+	position.y = wrapf(position.y, 0, screen_size.y)
+	
+	energy -= 1
+	if energy < 0:
+		queue_free()
 
 func _on_body_entered(body: Node2D) -> void:
 	queue_free()
