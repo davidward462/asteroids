b8647a78cb12ea59de1e90826262a2b029d0485a
David Ward <davidward462@users.noreply.github.com>
2025-03-07 14:09:44 -0800

    spawn player in center of viewport

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var screen_size = get_viewport_rect().end
+	var player_start_pos = Vector2(screen_size.x/2, screen_size.y/2)
 	var scene = load("res://scenes/player.tscn")
 	var player = scene.instantiate ()
 	add_child(player)
-	pass # Replace with function body.
+	player.position = player_start_pos
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
