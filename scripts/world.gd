3f7562fe22874ae07565fef16352b62706553637
David Ward <davidward462@users.noreply.github.com>
2025-03-08 23:23:11 -0800

    add player spawn func

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,14 +1,19 @@
 extends Node2D
 
+@onready var player_scene = load("res://scenes/player.tscn")
+
+func spawn_player(screen_size, start_pos) -> void:
+	var player = player_scene.instantiate ()
+	add_child(player)
+	player.position = start_pos
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var screen_size = get_viewport_rect().end
 	var player_start_pos = Vector2(screen_size.x/2, screen_size.y/2)
-	var scene = load("res://scenes/player.tscn")
-	var player = scene.instantiate ()
-	add_child(player)
-	player.position = player_start_pos
-
+	
+	# Spawn player in the center of the viewport.
+	spawn_player(screen_size, player_start_pos)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
