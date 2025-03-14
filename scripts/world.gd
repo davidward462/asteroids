1bacf026db9241ab2f82a8bbd4650f66a7d153fd
David Ward <davidward462@users.noreply.github.com>
2025-03-13 17:28:13 -0700

    spawn asteroid at random position in the world

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,19 +1,43 @@
 extends Node2D
 
 @onready var player_scene = load("res://scenes/player.tscn")
+@onready var asteroid_scene = load("res://scenes/asteroid.tscn")
+
+var asteroid_count = 1
 
 func spawn_player(screen_size, start_pos) -> void:
 	var player = player_scene.instantiate ()
 	add_child(player)
 	player.position = start_pos
+	
+func gen_asteroid_coords(screen_size) -> Vector2:
+	var buffer = 120
+	var screen_x = int(screen_size.x)
+	var screen_y = int(screen_size.y)
+	
+	var x = (randi() % (screen_x)-buffer ) + buffer
+	var y = (randi() % (screen_y)-buffer ) + buffer
+	var coords = Vector2(x, y)
+	print(coords)
+	return coords
+	
+func spawn_asteroid(pos) -> void:
+	print("spawn asteroid")
+	var asteroid = asteroid_scene.instantiate()
+	add_child(asteroid)
+	asteroid.position = pos
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	randomize()
 	var screen_size = get_viewport_rect().end
 	var player_start_pos = Vector2(screen_size.x/2, screen_size.y/2)
 	
 	# Spawn player in the center of the viewport.
 	spawn_player(screen_size, player_start_pos)
+	
+	# Spawn asteroids
+	spawn_asteroid(gen_asteroid_coords(screen_size))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
