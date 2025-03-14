0963949f5600c33152cbb4a067f513b70741250b
David Ward <davidward462@users.noreply.github.com>
2025-03-13 19:46:13 -0700

    spawn asteroids in a group

--- scripts/world.gd
+++ scripts/world.gd
@@ -2,15 +2,16 @@ extends Node2D
 
 @onready var player_scene = load("res://scenes/player.tscn")
 @onready var asteroid_scene = load("res://scenes/asteroid.tscn")
+@onready var screen_size = get_viewport_rect().end
 
-var asteroid_count = 1
+var asteroid_count = 5
 
-func spawn_player(screen_size, start_pos) -> void:
+func spawn_player(start_pos) -> void:
 	var player = player_scene.instantiate ()
 	add_child(player)
 	player.position = start_pos
 	
-func gen_asteroid_coords(screen_size) -> Vector2:
+func gen_asteroid_coords() -> Vector2:
 	var buffer = 120
 	var screen_x = int(screen_size.x)
 	var screen_y = int(screen_size.y)
@@ -21,23 +22,27 @@ func gen_asteroid_coords(screen_size) -> Vector2:
 	print(coords)
 	return coords
 	
-func spawn_asteroid(pos) -> void:
+func spawn_asteroid() -> void:
+	var pos = gen_asteroid_coords()
 	print("spawn asteroid")
 	var asteroid = asteroid_scene.instantiate()
 	add_child(asteroid)
 	asteroid.position = pos
+	
+func spawn_asteroids(count) -> void:
+	for i in range(count):
+		spawn_asteroid()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize()
-	var screen_size = get_viewport_rect().end
 	var player_start_pos = Vector2(screen_size.x/2, screen_size.y/2)
 	
 	# Spawn player in the center of the viewport.
-	spawn_player(screen_size, player_start_pos)
+	# spawn_player(screen_size, player_start_pos)
 	
 	# Spawn asteroids
-	spawn_asteroid(gen_asteroid_coords(screen_size))
+	spawn_asteroids(asteroid_count)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
